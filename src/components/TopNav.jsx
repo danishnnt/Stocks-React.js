764ec50fe8adfe8595e87ee12c2715e97c675848
Danish Khan <danish.zcz@gmail.com>
2024-03-21 12:50:21 +0530

    third

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Container, Flex, Menu , Heading, MenuButton, MenuItem, MenuList, Button } from "@chakra-ui/react";
+import React from "react";
 
 const TopNav = () => {
   return (
-    <div>TopNav</div>
-  )
-}
+    <Flex boxShadow="xl">
+      <Container>
+        <Heading>Dashboard</Heading>
+        <Menu>
+          <MenuButton as={Button}>
+            Actions
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+      </Container>
+    </Flex>
+  );
+};
 
-export default TopNav
+export default TopNav;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import SideNav from '../../components/SideNav'
+import TopNav from '../../components/TopNav'
+import { Flex } from '@chakra-ui/react'
 
 const Dashboard = () => {
   return (
-    <div>
+    
+      <Flex>
         <SideNav/>
-    </div>
+        <TopNav/>
+        </Flex>
+    
   )
 }
 
